86352d48227bdd87435fc2351e2c21c3047d474e
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-04-22 07:57:30 +0530

    Add counter functionality with increment and decrement instructions

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,17 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
+enum InstructionType {
+    Increment(u32),
+    Decrement(u32)
+}
+
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32
+}
+
 entrypoint!(counter_contract);
 
 pub fn counter_contract(
@@ -17,5 +28,20 @@ pub fn counter_contract(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?; 
+    let instruction = InstructionType::try_from_slice(instruction_data)?;
+    let mut current_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match instruction {
+        InstructionType::Increment(value) => {
+            current_data.count += 1;
+        }
+        InstructionType::Decrement(value) => {
+            current_data.count -= 1;
+        }
+    } 
+    current_data.serialize(&mut *acc.data.borrow_mut())?;
+
+    msg!("Updated Counter of {} to {:?}", acc.key, current_data);
     Ok(())
 }
